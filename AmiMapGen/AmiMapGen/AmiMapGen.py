6255dc178b8f507453e68390ec09e67efa2c738d
Daniel Ives <iveseyboy@hotmail.com>
2015-11-19 11:54:42 +0000

    factored out an AmiMapGenerator class for better re-usability

--- AmiMapGen/AmiMapGen/AmiMapGen.py
+++ AmiMapGen/AmiMapGen/AmiMapGen.py
@@ -5,23 +5,9 @@ import json
 import argparse
 import os
 
-def print_if_verbose(message, verbose):
-    """only print the message if the verbose arg is True"""
-    if verbose:
-        print(message)
-
 def csv(value):
     """split value based on commas. Used in argparser for include / exclude lists"""
     return value.split(',')
-
-def i_should_query_region(region_to_check, include, exclude):
-    """return True if we should be checking this region"""
-    if (include is None) & (exclude is None):
-        return True
-    if include != None:
-        return region_to_check in include
-    if exclude != None:
-        return region_to_check not in exclude
 
 DEFAULT_REGION = 'eu-west-1'
 """I'd call this the default default region"""
@@ -46,47 +32,89 @@ def main():
     parser.add_argument('-v', '--verbose',
                         help='verbose mode',
                         action='store_true')
-    include_exclude_group = parser.add_mutually_exclusive_group()
-    include_exclude_group.add_argument('-i', '--include',
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-i', '--include',
                        type=csv,
                        help='comma-separated list of regions to include (default is all)')
-    include_exclude_group.add_argument('-e', '--exclude',
+    group.add_argument('-e', '--exclude',
                        type=csv,
                        help='comma-separated list of regions to exclude (default is none)')
 
     args = parser.parse_args()
 
-    map_name = args.name
-    images = args.images
-    result = {map_name : {}}
-    iteration = 0
-
-    for image in images:
-        conn = boto.ec2.connect_to_region(args.region)
-        current_image = conn.get_image(image)
-        name = current_image.name
-
-        try:
-            key = args.keys[iteration]
-        except (IndexError, TypeError):
-            key = 'AMI{}'.format(iteration + 1)
-
-        print_if_verbose('Got: "{}" in "{}"'.format(name, conn.region.name), args.verbose)
-
-        for region in conn.get_all_regions():
-            if i_should_query_region(region.name, args.include, args.exclude):
-                conn = boto.ec2.connect_to_region(region.name)
-                region_images = conn.get_all_images(filters={'name':name})
-                for region_image in region_images:
-                    if conn.region.name not in result[map_name]:
-                        result[map_name][conn.region.name] = {}
-
-                    result[map_name][conn.region.name][key] = region_image.id
-                    print_if_verbose(
-                        'Got "{}" in "{}"'.format(region_image.id, conn.region.name), args.verbose)
-        iteration += 1
+    generator = AmiMapGenerator(args.region, args.verbose, args.include, args.exclude)
+    result = generator.generate_map(args.name, args.images, args.keys)
 
     print(json.dumps(result, indent=4))
+
+class AmiMapGenerator:
+    """class that does the RegionMap generation"""
+
+    def __init__(self, initial_region, verbose, include, exclude):
+        self.verbose = verbose
+        self.initial_region = initial_region
+        self.include = include
+        self.exclude = exclude
+        self.check_all = ((include is None) & (exclude is None))
+        self.region_cache = {}
+
+    def print_if_verbose(self, message):
+        """only print the message if the verbose attribute for this object is True"""
+        if self.verbose:
+            print(message)
+
+    def i_should_query_region(self, region_to_check):
+        """return True if we should be checking this region"""
+        if self.check_all:
+            return True
+        if region_to_check in self.region_cache:
+            return True
+        if self.include != None:
+            if region_to_check in self.include:
+                self.region_cache[region_to_check] = True
+                return True
+        if self.exclude != None:
+            if region_to_check not in self.exclude:
+                self.region_cache[region_to_check] = True
+                return True
+        return False
+
+    def generate_map(self, map_name, images, keys):
+        """recurse regions and amis to generate the map"""
+
+        result = {map_name : {}}
+        iteration = 0
+        regions = None
+
+        for image in images:
+            conn = boto.ec2.connect_to_region(self.initial_region)
+            current_image = conn.get_image(image)
+            name = current_image.name
+
+            try:
+                key = keys[iteration]
+            except (IndexError, TypeError):
+                key = 'AMI{}'.format(iteration + 1)
+
+            self.print_if_verbose('Got: "{}" in "{}"'.format(name, conn.region.name))
+
+            if regions is None:
+                regions = conn.get_all_regions()
+
+            for region in regions:
+                if self.i_should_query_region(region.name):
+                    conn = boto.ec2.connect_to_region(region.name)
+                    region_images = conn.get_all_images(filters={'name':name})
+                    for region_image in region_images:
+                        if conn.region.name not in result[map_name]:
+                            result[map_name][conn.region.name] = {}
+
+                        result[map_name][conn.region.name][key] = region_image.id
+                        self.print_if_verbose(
+                            'Got "{}" in "{}"'.format(region_image.id, conn.region.name))
+            iteration += 1
+
+        return result
 
 if __name__ == '__main__':
     main()
