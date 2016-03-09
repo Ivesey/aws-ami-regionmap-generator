6afd483bc7b2059d32772639595fd4b54e5aaf60
Daniel Ives <iveseyboy@hotmail.com>
2016-03-09 17:00:24 +0000

    updated to boto3

--- AmiMapGen/AmiMapGen/AmiMapGen.py
+++ AmiMapGen/AmiMapGen/AmiMapGen.py
@@ -1,6 +1,6 @@
 ﻿"""generates a CloudFormation region map for AMIs"""
 
-import boto.ec2
+import boto3
 import json
 import argparse
 import os
@@ -16,34 +16,32 @@ def main():
     """generates the region map and dumps it as JSON"""
     parser = argparse.ArgumentParser(description='generates a CloudFormation region map for AMIs')
     parser.add_argument('images',
-                        type=csv,
-                        help='comma-separated list of valid AMIs')
-    parser.add_argument('-k', '--keys',
-                        type=csv,
-                        help='comma-separated list of map keys for images (default is AMIx)')
+                        nargs='*',
+                        help='(list of) valid AMI image id(s)')
     parser.add_argument('-r', '--region',
                         help='specify the region if "image" is not in your default region',
-                        default=os.getenv('AWS_DEFAULT_REGION',
-                                          boto.config.get('Boto', 'ec2_region_name',
-                                                          DEFAULT_REGION)))
+                        default=os.getenv('AWS_DEFAULT_REGION', DEFAULT_REGION))
     parser.add_argument('-n', '--name',
                         help='specify a name for the region map (default is "RegionMap")',
                         default='RegionMap')
     parser.add_argument('-v', '--verbose',
                         help='verbose mode',
                         action='store_true')
+    parser.add_argument('-k', '--keynames',
+                        nargs='*',
+                        help='(list of) key name(s) (default is "AMIx")')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-i', '--include',
-                       type=csv,
-                       help='comma-separated list of regions to include (default is all)')
+                       nargs='*',
+                       help='list of regions to include (default is all)')
     group.add_argument('-e', '--exclude',
-                       type=csv,
-                       help='comma-separated list of regions to exclude (default is none)')
+                       nargs='*',
+                       help='list of regions to exclude (default is none)')
 
     args = parser.parse_args()
 
     generator = AmiMapGenerator(args.region, args.verbose, args.include, args.exclude)
-    result = generator.generate_map(args.name, args.images, args.keys)
+    result = generator.generate_map(args.name, args.images, args.keynames)
 
     print(json.dumps(result, indent=4))
 
@@ -57,13 +55,15 @@ class AmiMapGenerator:
         self.exclude = exclude
         self.check_all = ((include is None) & (exclude is None))
         self.region_cache = {}
+        self.ec2 = boto3.resource('ec2', region_name=initial_region)
+        self.client = boto3.client('ec2', region_name=initial_region)
 
     def print_if_verbose(self, message):
         """only print the message if the verbose attribute for this object is True"""
         if self.verbose:
             print(message)
 
-    def i_should_query_region(self, region_to_check):
+    def query_this_region(self, region_to_check):
         """return True if we should be checking this region"""
         if self.check_all:
             return True
@@ -83,36 +83,37 @@ class AmiMapGenerator:
         """recurse regions and amis to generate the map"""
 
         result = {map_name : {}}
-        iteration = 0
-        regions = None
 
-        for image in images:
-            conn = boto.ec2.connect_to_region(self.initial_region)
-            current_image = conn.get_image(image)
-            name = current_image.name
+        key_index = 0
+
+        for image_id in images:
+            ami = self.ec2.Image(image_id)
+            ami_name = ami.name
+            creation_date = ami.creation_date
+
+            self.print_if_verbose('Got: "{}" in "{}"'.format(ami_name, self.initial_region))
+
+            response = self.client.describe_regions()
 
             try:
-                key = keys[iteration]
-            except (IndexError, TypeError):
-                key = 'AMI{}'.format(iteration + 1)
+                key = keys[key_index]
+            except Exception as e:
+                key = 'AMI{}'.format(key_index + 1)
 
-            self.print_if_verbose('Got: "{}" in "{}"'.format(name, conn.region.name))
+            key_index += 1
 
-            if regions is None:
-                regions = conn.get_all_regions()
-
-            for region in regions:
-                if self.i_should_query_region(region.name):
-                    conn = boto.ec2.connect_to_region(region.name)
-                    region_images = conn.get_all_images(filters={'name':name})
-                    for region_image in region_images:
-                        if conn.region.name not in result[map_name]:
-                            result[map_name][conn.region.name] = {}
-
-                        result[map_name][conn.region.name][key] = region_image.id
-                        self.print_if_verbose(
-                            'Got "{}" in "{}"'.format(region_image.id, conn.region.name))
-            iteration += 1
+            for region in response['Regions']:
+                region_name = region['RegionName']
+                if self.query_this_region(region_name):
+                    self.client = boto3.client('ec2', region_name)
+                    images = self.client.describe_images(Filters=[{'Name':'name', 'Values':[ami_name]}])
+                    try:
+                        t = result[map_name][region_name]
+                    except KeyError as e:
+                        result[map_name][region_name] = {}
+                    image = images['Images'][0]['ImageId']
+                    result[map_name][region_name][key] = image
+                    self.print_if_verbose('Got "{}" in "{}"'.format(image, region_name))
 
         return result
 
